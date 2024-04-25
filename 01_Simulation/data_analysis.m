63fd77aeb19e385ee3602626498f61a24af1d980
Pedro4064 <pedrohlcruz@gmail.com>
2024-04-25 04:38:00 -0300

    [IMPLEMENTED] Full Wave simulation and Interpolation for data analysis

--- 01_Simulation/data_analysis.m
+++ 01_Simulation/data_analysis.m
@@ -1,7 +1,8 @@
-%%%%% Half Wave Controled - 30 Deg %%%%%
+%% %%%%%%%%%%%%%%%%%%%%% Half Bridge - 30 Deg %%%%%%%%%%%%%%%%%%%%% %%
 clear; clc;
 load('data/half_wave_controled_30deg.mat');
 half_wave_30_controled = ans;
+
 %% Voltage
 plot(half_wave_30_controled.Time, half_wave_30_controled.Data(:, 1), "LineWidth",2.5), hold on;
 plot(half_wave_30_controled.Time, half_wave_30_controled.Data(:, 2), "LineWidth",2.5,"LineStyle",":", "Color", '#fa7a2a'), hold off;
@@ -60,11 +61,10 @@ fp= P/S;
 
 present_data({P, S, fp}, ["Potência Ativa", "Potência Aparente", "Fator de Potência"])
 
-%%
-
-%%%%% Half Wave Controled - 60 Deg %%%%%
+%% %%%%%%%%%%%%%%%%%%%%% Half Bridge - 60 Deg %%%%%%%%%%%%%%%%%%%%% %%
 load('data/half_wave_controled_60deg.mat');
 half_wave_60_controled = ans;
+
 %% Voltage
 plot(half_wave_60_controled.Time, half_wave_60_controled.Data(:, 1), "LineWidth",2.5), hold on;
 plot(half_wave_60_controled.Time, half_wave_60_controled.Data(:, 2), "LineWidth",2.5, "Color", '#fa7a2a', 'LineStyle',':'), hold off;
@@ -122,3 +122,31 @@ S = measurements_values{8} * measurements_values{2};
 fp= P/S;
 
 present_data({P, S, fp}, ["Potência Ativa", "Potência Aparente", "Fator de Potência"])
+
+%% %%%%%%%%%%%%%%%%%%%%% Full Bridge - 30 Deg Single Phase %%%%%%%%%%%%%%%%%%%%% %%
+% Since the simulation was done on a variable step size, it is necessary to
+% create a linear interpolation of fixed step size so all the calculations
+% are correct.
+load("data/full_wave_controled_30deg.mat");
+full_wave_30_deg = ans;
+
+fixed_step_time = min(full_wave_30_deg.Time):0.00001:max(full_wave_30_deg.Time);
+original_time = full_wave_30_deg.Time;
+data = full_wave_30_deg.Data;
+
+fixed_step_data = interp1(original_time, data, fixed_step_time);
+
+%% Voltage
+plot(fixed_step_time, fixed_step_data(:,1), "LineWidth",2.5), hold on;
+plot(fixed_step_time, fixed_step_data(:,2), "LineWidth",2.5,"LineStyle",":", "Color", '#fa7a2a'), hold off;
+
+xlim([0 5*(1/60)]);
+ylim([-300 300]);
+
+set(gcf,'Position',[100 100 1000 500])
+legend('V_{Source}', 'V_{Load}', fontsize=12);
+xlabel('Time (s)', fontsize=12);
+ylabel('Voltage (V)', fontsize=12);
+grid on;
+
+print(gcf,'-djpeg','-r900', 'imgs/[FULL WAVE - Controled] 30deg Voltages.jpeg');
